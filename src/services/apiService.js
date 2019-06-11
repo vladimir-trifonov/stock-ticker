465c550f894aa2b06e9504e200c1a629c57ebdb0
Vladimir Trifonov <trifonov.dev@gmail.com>
2019-06-11 19:31:01 +0300

    Update apiService.js

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,12 +1,12 @@
 'use strict'
 const rp = require('request-promise')
 
-const getTicker = async (symbol) => rp({
+const getTicker = (symbol) => rp({
   url: `${process.env.BITFINEX_URL}/ticker/${symbol}`,
   json: true
 })
 
-const getTickers = async () => rp({
+const getTickers = () => rp({
   url: `${process.env.BITFINEX_URL}/tickers?symbols=ALL`,
   json: true
 })
